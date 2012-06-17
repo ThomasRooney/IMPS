ba817389a9d2f09ccdc6fc875e499989d40cc29c
Terence Tse <tt1611@visual21.doc.ic.ac.uk>
2012-06-17 15:24:30 +0100

    ppm file now able to be assembled jsut the initial output has a wrong byte somewhere, checking

--- graphics/checkerboard.s
+++ graphics/checkerboard.s
@@ -9,8 +9,9 @@ pixel:            .fill 8
 char_P:           .fill 80
 char_6:           .fill 54
 char_4:           .fill 52
-char_space:       .fill 20
-max_colour:       .fill 255
+char_space:       .fill 32
+char_2:           .fill 50
+char_5:           .fill 53
 
 
 - use registers $4, $5, $6 for width, height, size
@@ -23,24 +24,22 @@ max_colour:       .fill 255
 - register $15 for 6
 - register $16 for 4
 - register $17 for space
+- register $18 for 2
+- register $19 for 5
 
 
-
-
-  
-
+colour_change:    beq  $7  $0  b_to_w          - changes the colour
+                  beq  $7  $19 w_to_b 
 b_to_w:
                   lw  $7  $0  white
                   lw  $8  $0  white
                   lw  $9  $0  white
-                  jmp end_row_pix    
-
+                  jr  $31
 w_to_b:
                   lw  $7  $0  black
                   lw  $8  $0  black
-                  lw  $9  $0  black
-                  jmp end_row_pix                     
-
+                  lw  $9  $0  black                   
+                  jr  $31
 
 start:            lw  $1  $0  white           - white is the first colour
                   lw  $4  $0  width           - register 4 = width
@@ -49,13 +48,14 @@ start:            lw  $1  $0  white           - white is the first colour
                   lw  $14 $0  char_P          - loads P into $14 
                   lw  $15 $0  char_6          - loads character 6 into $15 
                   lw  $16 $0  char_4          - loads character 4 to $16
+                  lw  $18 $0  char_2          - loads char 2
+                  lw  $19 $0  char_5          - loads char 5
                   lw  $17 $0  char_space      - load space character to $17	
 
                   addi $10 $0  1              - register 10 = col
                   addi $11 $0  1              - register 11 = col_pix
                   addi $12 $0  1              - register 12 = row
-                  addi $13 $0  1              - register 13 = row_pix	
-                  addi $19 $0  255            - register 13 = 255	           
+                  addi $13 $0  1              - register 13 = row_pix		           
 
                   out $14
                   out $15                     - outputs P6
@@ -69,8 +69,9 @@ start:            lw  $1  $0  white           - white is the first colour
                   out $16                     - print height
                   out $17
 
-                  lw  $18 max_colour
-                  out $18                     - prints 255
+                  out $18
+                  out $19                     - prints 255
+                  out $19
                   out $17
 
 print_pixel:      out $7
@@ -85,15 +86,11 @@ for_row_pix:      ble  $13 $6  colour_change
                   jal  print_pixel            - prints pixel out
                   addi $13 $13 1              - row_pix++ 
                   jmp  for_row_pix            - loop row_pix
-
-
-change_colour:    beq  $7  $0  b_to_w          - changes the colour
-                  beq  $7  $19 w_to_b
-
+                  jal  colour_change          - changes the colour
 end_row_pix:      addi $12 $12 1              - row++
                   jmp  for_row                - loop row
-end_row:          jal  colour_change          - corrects colour change of last iteration
-                  addi $11 $11 1- col_pix++		
+end_row:          jal colour_change           - corrects colour change of last iteration  
+                  addi $11 $11 1              - col_pix++		
                   jmp  for_col_pix            - loop col_pix
 end_col_pix:      addi $10 $10 1              - col++
                   jmp  for_col                - loop col
