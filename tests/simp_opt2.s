cd1844c2e57077245037e155404e31a60e7be4f7
U-TOMR-LAPTOP\Tom <Tom@T-Laptop.(none)>
2012-06-18 21:38:03 +0100

    Updated with more waste in test optimisation

--- tests/simp_opt2.s
+++ tests/simp_opt2.s
@@ -1,20 +1,15 @@
-start:  addi $0 $1 n1
-	addi $0 $1 n1
-	addi $0 $1 n1
-	addi $0 $1 n1
-	addi $0 $1 n1
-	addi $0 $1 n1
-	addi $0 $1 n1
-	addi $0 $1 n1
-	addi $0 $1 n1
-	addi $2 $20 72
-	out $2
-	addi $2 $20 101
-	out $2
-	addi $2 $20 121
-	out $2
+start:  addi $4 $0 n9
+    jmp waste
+end:  addi $2 $20 72
+	  out $2
+	  addi $2 $20 101
+	  out $2
+	  addi $2 $20 121
+	  out $2
 		
 n1: .fill 1
 n2: .fill 68
+n9: .fill 1000
 
-
+waste: addi $3 $3 1
+       bgt $3 $9 end
